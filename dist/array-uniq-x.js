e6599e648319ff9e6a0c1df2cb1b0562cf69e52b
Graham Fairweather <xotic750@gmail.com>
2019-08-20 11:56:12 +0200

    Maintenance

--- dist/array-uniq-x.js
+++ dist/array-uniq-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2019-present",
-  "date": "2019-08-18T14:19:57.156Z",
+  "date": "2019-08-20T09:56:05.460Z",
   "describe": "",
   "description": "Creates a duplicate-free version of an array, using SameValueZero for equality comparisons.",
   "file": "array-uniq-x.js",
